b2ae36e55ae144f0417352b0cd4df63f2bba2138
InshaM <42205104+InshaM@users.noreply.github.com>
2018-09-13 15:04:59 -0400

    Update lab6_q5.cpp

--- lab6_q5.cpp
+++ lab6_q5.cpp
@@ -1,8 +1,8 @@
-//including libabry
+//include in the  library .
 #include<iostream>
 using namespace std;
 
-//funcion that has two int parameters
+//function that has two int parameters.
 int func(int a, int b,int &c ){
 	if(c == 1){
 		int sum = a + b;
@@ -20,17 +20,17 @@ int func(int a, int b,int &c ){
 	}
 	
 }
-// drive fuction
+// drive function :
 int main(){int p,q,input;
-	//ask the user for two numners
+	//asking  the user for any two numbers he wishes .
 	cout<< "Let the numbers be : ";
 	cin>>p>>q;
-	//ask the user for the operation
+	//asking  the user for the operation .
 	cout<<"Which operation do you want?"<<endl<<"For sum:type 1"<<endl<<"For maximum between the two numbers: type 2"<<endl<<"For minimum between the two numbers: type 3"<<endl;
 	cin>>input;
 	// tell user the minimum
 	cout<<"The minimum among the numbers "<<p<< " and " << q <<" is "<<
-	//call the function with number for arguments 
+	//call the function with number for arguments  .
 	func(p,q,input) <<endl;
 	
        
