455631420598d4e8c5fc02c5cc009067dafc2cbd
InshaM <42205104+InshaM@users.noreply.github.com>
2018-09-13 15:00:22 -0400

    Update lab6_q4a.cpp

--- lab6_q4a.cpp
+++ lab6_q4a.cpp
@@ -1,23 +1,23 @@
-//including libabry
+//include in the library
 #include<iostream>
 using namespace std;
 
-//funcion that has two int parameters
+//Give the required function that has two int parameters.
 int func(int a, int b ){
 	if(a>b){
 	return b;}
 	else {
 	return a;}
 }
-// drive fuction
+// drive function :
 int main(){int p,q;
-	//ask the user for two numners
+	//ask the user for any two numbers he wants :
 	cout<< "Let the numbers be : ";
 	cin>>p>>q;
-	//call the function with number for arguments
+	//call the function with number for arguments :
 	func(p,q);
-	// tell user the minimum
-	cout<<"The minimum among the numbers "<<p<< " and " << q <<" is "<< func(p,q) <<endl;
+	// tell user the minimum :
+	cout<<"The minimum among the numbers "<< p << " and " << q <<" is "<< func(p,q) <<endl;
 	
        
 return 0;
