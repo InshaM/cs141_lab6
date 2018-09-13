93445e1a46a1e3495f6bfff4857ef2012cf8888a
InshaM <42205104+InshaM@users.noreply.github.com>
2018-09-13 14:44:47 -0400

    Update lab6_q2a.cpp

--- lab6_q2a.cpp
+++ lab6_q2a.cpp
@@ -1,21 +1,21 @@
-//including libabry
+//including in the libraby.
 #include<iostream>
 using namespace std;
 
-//funcion that has two int parameters
+//Giving the funcion that has two int parameters.
 int func(int a, int b){
 	// showing their sum
 	int sum = a + b;
 	return sum;
 }
-// drive fuction
+// The drive function
 int main(){int c,d;
-	//ask the user for two numners
+	//ask the user for any two numbers he likes.
 	cout<< "Let the numbers be : ";
 	cin>>c>>d;
-	// tell user the sum
-	cout<<"The sum of the numbers "<<c<< " and " << d <<" is "<< 
-	//call function for numbers as arguments
+	// tell user the required sum .
+	cout<<"The sum of the numbers "<<c<< " and " << d <<" is : "<< 
+	//call the function for numbers as arguments.
         func(c,d) <<endl;
 return 0;
 }
