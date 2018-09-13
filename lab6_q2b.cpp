096d52658d231493ee4db8f6b3262561179a4ec4
InshaM <42205104+InshaM@users.noreply.github.com>
2018-09-13 14:48:58 -0400

    Update lab6_q2b.cpp

--- lab6_q2b.cpp
+++ lab6_q2b.cpp
@@ -1,20 +1,20 @@
-//including libabry
+//including in the libabry
 #include<iostream>
 using namespace std;
 
-//funcion that has two int parameters
+//Giving the function that has two int parameters .
 void funcR(int a, int b ,int &c){
-	// showing the maximum
+	// Now , showing the maximum .
 	c=a+b;
 }
-// drive fuction
+// drive function .
 int main(){int p,q,sum;
 	//ask the user for two numners
 	cout<< "Let the numbers be : ";
 	cin>>p>>q;
-	//call the number for arguments
+	//call the number for arguments.
 	funcR(p,q,sum);
-	// tell user the sum
+	// telling the user the sum
 	cout<<"The sum of the numbers "<<p<< " and " << q <<" is "<<  sum <<endl;
 	
        
