2a8fda5deada5e3e291c89b65ef929196c0cd0e4
InshaM <42205104+InshaM@users.noreply.github.com>
2018-09-13 14:37:32 -0400

    Update lab6_q1.cpp

--- lab6_q1.cpp
+++ lab6_q1.cpp
@@ -1,7 +1,7 @@
-//include library
+//including in the library.
 #include <iostream>
 using namespace std;
-//funtion that include cost of the single unit,no of units, and tax rate and returns the value for calling modules
+//give the function that include the cost of the single unit,no of units, tax rate and returns the value for calling modules.
 void func1(int &unitCst,int &units,int &taxRt){
 	cout<<"The cost of the single unit : ";
 	cin>>unitCst;
@@ -12,13 +12,13 @@ void func1(int &unitCst,int &units,int &taxRt){
 	cout<<endl;
 }
 
-//function receives cost of the single unit,no of units, and tax rate and returns to the calling module the sales Tax and total cost.
+//The function that receives cost of the single unit,no of units, and tax rate and returns to the calling module the sales Tax and total cost.
 void func2(int unitCst,int units, int taxRt, int &salesTx ,int &totdue){
 	salesTx = (units*unitCst*taxRt/100);
 	totdue = units*unitCst + salesTx;
 } 
 
-//function receives cost of the single unit,no of units,tax rate ,the sales Tax and total cost and outputs in readable format
+//Give the function that receives cost of the single unit,no of units,tax rate ,the sales Tax and total cost and outputs in readable format
 void func3(int unitCst,int units, int taxRt, int salesTx ,int totdue){
 	cout<<"The cost of the single unit : "<<unitCst<<endl;
 	cout<<"The total number of units : "<<units<<endl;
@@ -26,7 +26,7 @@ void func3(int unitCst,int units, int taxRt, int salesTx ,int totdue){
 	cout<<"The sales tax of the unit : "<<salesTx<<endl;
 	cout<<"The total cost of the "<< units<<" unit : "<<totdue <<endl;
 } 
-//main function with variables called unitCost, unitsPurch, taxRate, salesTax, and totalDue.
+//Give the required main function with the variables called unitCost, unitsPurch, taxRate, salesTax, and totalDue.
 int main(){
 	int unitCst,units,taxRt,salesTx,totdue;	
 	//declaring all variables
